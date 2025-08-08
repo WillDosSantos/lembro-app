f192387f998a493174308825c525c891c7058dac
William dos Santos <willdossantosdesigns@gmail.com>
2025-08-08 11:54:51 -0600

    edit profile features

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,19 @@
+import { writeFile } from "fs/promises";
+import path from "path";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const formData = await req.formData();
+  const file: File | null = formData.get("file") as unknown as File;
+
+  if (!file) {
+    return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
+  }
+
+  const bytes = await file.arrayBuffer();
+  const buffer = Buffer.from(bytes);
+  const filePath = path.join(process.cwd(), "public", "uploads", file.name);
+  await writeFile(filePath, buffer);
+
+  return NextResponse.json({ filename: file.name });
+}
